ceaccc6b69817fcdc157c8c8002279479d1ccf04
Ayush Chandra Gavara <ayushchandragavara@gmail.com>
2026-02-15 23:02:10 +0530

    Updated main.c

--- main.c
+++ main.c
@@ -33,3 +33,24 @@ void generate_files() {
     system("ps aux > x1.txt");
     system("ps -eLf > x2.txt");
 }
+
+void merge_files() {
+
+    FILE *fp1, *fp2, *out;
+    char line[MAX_LINE];
+    struct process_info p;
+    int pid2;
+
+    fp1 = fopen("x1.txt", "r");
+    fp2 = fopen("x2.txt", "r");
+    out = fopen("merged.txt", "w");
+
+    if (!fp1 || !fp2 || !out) {
+        printf("Error opening files\n");
+        exit(1);
+    }
+
+    fgets(line, MAX_LINE, fp1);
+    fgets(line, MAX_LINE, fp2);
+
+    fprintf(out,"USER\tPID\tLWP\tNLWP\tCPU\tMEM\tCOMMAND\n");
